87a86c56da47e2d5d4d34177430e701d24df8f2c
Pranjal Kamboj <pranjalkamboj007@gmail.com>
2023-05-07 20:30:21 +0530

    first commit

---
+++ lesson1.ts
@@ -0,0 +1,13 @@
+//variables declarations
+
+let myString: string = "Hello, world!"; // variable of type string
+const myNumber: number = 42; // variable of type number
+
+//Type Inference
+//type of a variable can be inferred from its value 
+//if you don't specify a type explicitly
+let myString2 = "Hello, world!"; // type inferred as string
+const myNumber2 = 42; // type inferred as number
+
+
+
